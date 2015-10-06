3f3b3e4b233b8a3a9ff0cb711626adebffea4b6c
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-05 21:09:57 -0500

    Oops! Price structure is based on ingredients not on cereals.

--- src/com/amdudda/Price.java
+++ src/com/amdudda/Price.java
@@ -8,33 +8,25 @@ import java.util.HashMap;
 public class Price {
     // class to store and retrieve cost info
 
-    private String product;
-    private Cereal cereal;
+    private String ingredient;
     private HashMap <Integer,Double> priceinfo;
     // the HashMap stores an integer, which is the cutoff quantity for a given price
 
     // Constructor
-    public Price(Cereal c){
-        this.cereal = c;
-        this.product = c.getName();  // redundant but ease of reference is good
+    public Price(String ing){
+        this.ingredient = ing;
         this.priceinfo = new HashMap <Integer,Double>();
     }
 
     // a bunch of getters and setters
-    public String getProduct() {
-        return product;
+
+
+    public String getIngredient() {
+        return ingredient;
     }
 
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public Cereal getCereal() {
-        return cereal;
-    }
-
-    public void setCereal(Cereal cereal) {
-        this.cereal = cereal;
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
     }
 
     public HashMap<Integer, Double> getPriceinfo() {

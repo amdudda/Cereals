e142c0490fb7553b4071f894ecab883d181cd292
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-06 18:18:49 -0500

    Modified getData() to read in data from recipies.txt

--- src/com/amdudda/Main.java
+++ src/com/amdudda/Main.java
@@ -3,8 +3,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -55,16 +56,50 @@ public class Main {
     }
 
 
-    public static ArrayList<Cereal> getData(){
+    public static ArrayList<Cereal> getData() throws IOException {
+        // Read in data from recipies.txt and create our cereals from that.
+        // set up our data streams
+        File f = new File("./data/recipies.txt");
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+
+        // read in our first line of data
+        String c_info = br.readLine();
+
+        // set up variables to hold the data we read in
+        int eod;
         ArrayList<Cereal> c_list = new ArrayList<Cereal>();
-        Cereal rc = new Cereal("Rice Crunchies", 0, 400, 20, 1, 0);
-        c_list.add(rc);
-        Cereal mf = new Cereal("Morning Flakes",350, 0, 40, 1, 0);
-        c_list.add(mf);
-        Cereal tz = new Cereal("Trianglz", 200, 200, 35, .5, 0);
-        c_list.add(tz);
-        Cereal h = new Cereal("Hoops", 120, 0, 25, 1, 350);
-        c_list.add(h);
+        ArrayList<Double> cereal = new ArrayList<Double>();
+        Integer arrsize = Integer.parseInt(c_info.substring(c_info.indexOf(":")+1));
+
+        // next line of the file contains our first row of data
+        c_info = br.readLine();
+
+        // and start processing it
+        while (!(c_info.equals("/*")) && c_info != null ) {
+            eod = c_info.indexOf(",");
+            String cname = c_info.substring(0,eod);
+            c_info = c_info.substring(eod+1);
+
+            for (int i=0; i<arrsize; i++) {
+                if ( c_info.indexOf(",") == -1 ) { eod = c_info.length(); }
+                else { eod = c_info.indexOf(","); }
+                Double amt = Double.parseDouble(c_info.substring(0, eod));
+                cereal.add(amt);
+                if (c_info.indexOf(",") != -1) c_info = c_info.substring(eod + 1);
+            }
+            Cereal nc = new Cereal(cname,cereal.get(0),cereal.get(1),cereal.get(2),
+                    cereal.get(3), cereal.get(4));
+
+            // move to the next line
+            c_info = br.readLine();
+        } // end while
+
+        // close our datastreams
+        br.close();
+        fr.close();
+
+        // and return our array
         return c_list;
     }
 
